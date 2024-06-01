456f99953b34239e5915b027df1af3e376a62a94
N-Rem <nicolasromero.barrios@gmail.com>
2024-06-01 13:56:53 -0300

    Agrega Instrucciones en Config.js

--- config.js
+++ config.js
@@ -9,3 +9,13 @@ export const CONFIG = {
   proxyUrl: '/blog',
   defaultPort: 8000,
 };
+
+//! Para que funcione hay que instalar las librerías necesarias:
+//? npm i 
+//! en caso de error:
+//? npm i -force
+
+//!Para activar el servidor:
+//? node server.js
+//! se aloja en el puerto 8000
+
